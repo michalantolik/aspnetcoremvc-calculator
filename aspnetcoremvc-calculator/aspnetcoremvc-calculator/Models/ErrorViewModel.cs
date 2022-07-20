c63c79d77e938cc7f917a056313ef25068d03ca8
Michał Antolik (MANT) <mant@demant.com>
2022-07-20 08:50:42 +0200

    Add empty solution

---
+++ aspnetcoremvc-calculator/aspnetcoremvc-calculator/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace aspnetcoremvc_calculator.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
